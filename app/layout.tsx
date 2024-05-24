74d3965b92ed940d851c7463a886d144da7df00f
Gambhir ⚡ <gambhir786191ss@gmail.com>
2024-05-25 02:37:51 +0530

    add: web-analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import TopNav from "./components/TopNav";
 import Footer from "./components/Footer";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,11 +21,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="lg:mx-40 lg:mt-8 md:mx-32 md:mt-4 mx-4 mt-3">
-        <TopNav />
-        {children}
-          <Footer/>
-      </div>
-      {/* <div className="bg-[url('../public/gradient.png')] bg-contain w-full h-10"></div> */}
+          <TopNav />
+          {children}
+          <Analytics />
+          <Footer />
+        </div>
+        {/* <div className="bg-[url('../public/gradient.png')] bg-contain w-full h-10"></div> */}
       </body>
     </html>
   );
