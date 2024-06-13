d8728460921a781cb9ef148ad8eaf1ba68d87487
Gambhir ⚡ <gambhir786191ss@gmail.com>
2024-06-13 22:48:14 +0530

    remove: google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import TopNav from "./components/TopNav";
 import Footer from "./components/Footer";
 import { Analytics } from '@vercel/analytics/react';
-import GoogleAnalytics from "./about/utils/GoogleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +19,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleAnalytics/>
       <body className={inter.className}>
         <div className="lg:mx-40 lg:mt-8 md:mx-32 md:mt-4 mx-4 mt-3">
           <TopNav />
